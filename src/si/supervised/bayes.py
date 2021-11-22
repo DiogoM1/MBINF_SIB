77b3a1a7a05c61ce9291bb2446a24aa897869f66
DioDuPiMa <diogodpmacedo@gmail.com>
2021-11-22 16:33:25 +0000

    added bayes

---
+++ src/si/supervised/bayes.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from si.supervised.supervised_model import SupervisedModel
+from si.util.distance import euclidian_distance
+from si.util.metrics import accuracy
+
+
+class NaiveBayes(SupervisedModel):
+    def __init__(self, k, distance_func=euclidian_distance, classification=True):
+        super().__init__()
+        self.k = k
+        self.classification = classification
+        self._distance_func = distance_func
+
+    def fit(self, dataset):
+        if not dataset.hasLabel():
+            raise Exception("Data has no labels.")
+        self.data = dataset
+        self.is_fitted = True
+        return self.data
+
+    def get_neighbours(self, x):
+#        dist = np.ma.apply_along_axis(self._distance_func, axis=0, arr=self.data.X, y=x)
+#        dist = self._distance_func(self.data.X, x)
+        distances = self._distance_func(self.data.X, x)
+        a = np.argsort(distances)[:self.k]
+        return a
+
+    def predict(self, x):
+        if not self.is_fitted:
+            raise Exception("The model hasn't been fitted yet.")
+        neighbours = self.get_neighbours(x)
+        i = neighbours.tolist()
+        meta_data = self.data.y[i].tolist()
+        if self.classification:
+            prediction = max(set(meta_data), key=meta_data.count)
+        else:
+            prediction = sum(meta_data) / len(meta_data)
+        return np.array(prediction)
+
+    def cost(self):
+        if not self.is_fitted:
+            raise Exception("The model hasn't been fitted yet.")
+        y_pred = np.ma.apply_along_axis(self.predict, axis=0, arr=self.data.X.T)
+        return accuracy(self.data.y, y_pred)
