5131f33fc0cd4b5ae7a7ebd72784e4413838cf1d
DioDuPiMa <diogodpmacedo@gmail.com>
2021-11-29 16:50:28 +0000

    - added the professor version of Decision Trees - added eval3 file - added the Ensemble class - added the necessary imports to __init__.py

--- src/si/supervised/__init__.py
+++ src/si/supervised/__init__.py
@@ -2,3 +2,5 @@ from .knn import KNN
 from .linreg import LinearRegression, LinearRegressionReg
 from .logreg import LogisticRegression, LogisticRegressionReg
 from .bayes import NaiveBayes
+from .dt import DecisionTree
+from .ensemble import Essemble
