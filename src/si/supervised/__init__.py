99521d01fe724f9102fc2c015bf7a2ce768a26f6
DioDuPiMa <diogodpmacedo@gmail.com>
2021-12-06 16:20:16 +0000

    added init

--- src/si/supervised/__init__.py
+++ src/si/supervised/__init__.py
@@ -4,3 +4,7 @@ from .logreg import LogisticRegression, LogisticRegressionReg
 from .bayes import NaiveBayes
 from .dt import DecisionTree
 from .ensemble import Essemble
+
+# TODO: Add function or class for confusion matrix
+# Fazer class para ter graficos (callable) -> __call__
+# Added weighted ensamble
