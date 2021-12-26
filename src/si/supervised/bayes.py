65a29d2e93a8ca86c92d53282bc6a8e843778398
DioDuPiMa <diogodpmacedo@gmail.com>
2021-12-26 17:29:26 +0000

    done

--- src/si/supervised/bayes.py
+++ src/si/supervised/bayes.py
@@ -3,43 +3,39 @@ import numpy as np
 from si.supervised.supervised_model import SupervisedModel
 from si.util.distance import euclidian_distance
 from si.util.metrics import accuracy
-
+from si.util.train import vectorize_dataset
 
 class NaiveBayes(SupervisedModel):
-    def __init__(self, k, distance_func=euclidian_distance, classification=True):
+    def __init__(self, k, classification=True):
         super().__init__()
         self.k = k
         self.classification = classification
-        self._distance_func = distance_func
 
     def fit(self, dataset):
         if not dataset.hasLabel():
             raise Exception("Data has no labels.")
-        self.data = dataset
+        self.labels = np.unique(dataset.y)
+        prob_matrix = {}
+        for label in self.labels:
+            prob_matrix[label] = [np.unique(dataset.X[dataset.y == label, i], return_counts=True) for i, _ in enumerate(dataset.xnames)]
+            prob_matrix[label] = [dict(zip(i[0], i[1]/i[1].sum())) for i in prob_matrix[label]]
+        self.prob_matrix = prob_matrix
         self.is_fitted = True
-        return self.data
-
-    def get_neighbours(self, x):
-#        dist = np.ma.apply_along_axis(self._distance_func, axis=0, arr=self.data.X, y=x)
-#        dist = self._distance_func(self.data.X, x)
-        distances = self._distance_func(self.data.X, x)
-        a = np.argsort(distances)[:self.k]
-        return a
+        return self.prob_matrix
 
     def predict(self, x):
         if not self.is_fitted:
             raise Exception("The model hasn't been fitted yet.")
-        neighbours = self.get_neighbours(x)
-        i = neighbours.tolist()
-        meta_data = self.data.y[i].tolist()
-        if self.classification:
-            prediction = max(set(meta_data), key=meta_data.count)
-        else:
-            prediction = sum(meta_data) / len(meta_data)
-        return np.array(prediction)
+        prob = {}
+        for label in self.labels:
+            prob[label] = 1
+            for col, val in enumerate(x):
+                prob[label] *= self.prob_matrix[label][col].get(val, 0)
+        choice = max(prob, key=prob.get)
+        return choice
 
-    def cost(self):
+    def cost(self, X=None, y=None):
         if not self.is_fitted:
             raise Exception("The model hasn't been fitted yet.")
-        y_pred = np.ma.apply_along_axis(self.predict, axis=0, arr=self.data.X.T)
-        return accuracy(self.data.y, y_pred)
+        y_pred = np.ma.apply_along_axis(self.predict, axis=0, arr=X.T)
+        return accuracy(y, y_pred)
